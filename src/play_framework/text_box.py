662f568398f12d432d0461ba6454ed13aa26510c
nopeless <38830903+nopeless@users.noreply.github.com>
2023-10-28 17:01:12 -0500

    text box bug fix

--- src/play_framework/text_box.py
+++ src/play_framework/text_box.py
@@ -4,8 +4,7 @@ import pygame
 class TextBox(pygame.sprite.Sprite):
     def __init__(self, x, y, width, height, text="", input=False):
         super().__init__()
-        self.image = pygame.Surface((width, height))
-        self.image.fill((255, 255, 255))
+        self.image = pygame.Surface((width, height)).convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -14,10 +13,11 @@ class TextBox(pygame.sprite.Sprite):
         self._text = text
 
     def _render_text(self):
-        text_surface = self.font.render(self._text, True, (0, 0, 0))
-
         if not self.image:
             raise ValueError("Image is not set")
+
+        self.image.fill((0, 0, 0, 0))
+        text_surface = self.font.render(self._text, True, (0, 0, 0))
 
         text_rect = text_surface.get_rect(center=self.image.get_rect().center)
         self.image.blit(text_surface, text_rect)
